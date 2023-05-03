b646e9918bc3bea27c0b7d87822125dfc49530e2
MOUNCIF <Mons-L@users.noreply.github.com>
2023-05-03 15:56:20 +0200

    main file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../components/mynavbar/style.mynavbar.css'
+import '../components/footer/style.footer.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
