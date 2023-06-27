eb7d5d0ce8416a5827b05327d443b3a3709c65e5
MOUNCIF <Mons-L@users.noreply.github.com>
2023-06-27 20:53:12 +0200

    Formulas and formulas full informations endpoints

--- src/database/formulas.db.service.js
+++ src/database/formulas.db.service.js
@@ -1,18 +1,19 @@
 import db from './db.service';
 
 const SELECT_FORMULA = 'SELECT * FROM formula';
+const SELECT_FORMULA_BY_TYPE_ID = 'SELECT * FROM formula WHERE formula_type_id=?';
 const SELECT_FORMULA_BY_ID = 'SELECT * FROM formula WHERE id=?';
-const INSERT_FORMULA = 'INSERT INTO formula(label, description, type) VALUES (?, ?, ?)';
-const UPDATE_FORMULA = 'UPDATE formula SET label=?, description=?, type=? WHERE id=?';
+const INSERT_FORMULA = 'INSERT INTO formula(label, description, formula_type_id) VALUES (?, ?, ?)';
+const UPDATE_FORMULA = 'UPDATE formula SET label=?, description=?, formula_type_id=? WHERE id=?';
 const DELETE_FORMULA = 'DELETE FROM formula WHERE id=?';
 
 module.exports = {
-    async getVehiclesTypes(){
+    async getFormulas(){
         try {
-            return await db.query(SELECT_VEHICLE_TYPE);
+            return await db.query(SELECT_FORMULA);
         }
         catch(err){
-            const message = '[vehiclesTypes.db.service] Getting vehicles types went bad. Reason: ' + err;
+            const message = '[formulas.db.service] Getting formulas went bad. Reason: ' + err;
             console.error(message);
             throw message;
         }
@@ -21,15 +22,29 @@ module.exports = {
         }
     },
 
-    async getVehicleTypeById(id){
+    async getFormulasByType(typeId){
         try {
-            const result = await db.query(SELECT_VEHICLE_TYPE_BY_ID, [id]);
+            return await db.query(SELECT_FORMULA_BY_TYPE_ID, [typeId]);
+        }
+        catch(err){
+            const message = '[formulas.db.service] Getting formulas went bad. Reason: ' + err;
+            console.error(message);
+            throw message;
+        }
+        finally {
+            await db.end();
+        }
+    },
+
+    async getFormulaById(id){
+        try {
+            const result = await db.query(SELECT_FORMULA_BY_ID, [id]);
             if(result.length !== 1)
                 return null;
             return result[0];
         }
         catch(err){
-            const message = '[vehiclesTypes.db.service] Getting vehicle type went bad. Reason: ' + err;
+            const message = '[formulas.db.service] Getting formula with id went bad. Reason: ' + err;
             console.error(message);
             throw message;
         }
@@ -38,20 +53,25 @@ module.exports = {
         }
     },
 
-    async insertVehcileType(label, description){
+    async insertFormula(label, description, formulaTypeId, transaction){
+        let queryMethod = transaction? await db.queryTransaction : db.query
         try{
-            const result = await db.query(INSERT_VEHICLE_TYPE, [label, description]);
+            let result = await queryMethod(INSERT_FORMULA, [label, description, formulaTypeId], transaction);
 
-            if(result.affectedRows !== 1){
-                console.error("Insert vehicle_type went bad. The expected number of affected rows is wrong."
-                    + " Expected: 1, affected rows: " + result.affectedRows);
-                return false;
+            if(transaction)
+                return result
+            else{
+                if(result.affectedRows !== 1){
+                    console.error("Insert formula went bad. The expected number of affected rows is wrong."
+                        + " Expected: 1, affected rows: " + result.affectedRows);
+                    return false;
+                }
+                return result.insertId;
             }
-            return result.insertId;
         }
         catch(err){
-            const message = '[vehiclesTypes.db.service] Inserting vehicle type with values { label: '
-                + label + ', description: ' + description + ' } went bad. Reason: ' + err;
+            const message = '[formulas.db.service] Inserting formula with values { label: '
+                + label + ', description: ' + description + ', formulaTypeId: ' + formulaTypeId + ' } went bad. Reason: ' + err;
 
             console.error(message);
             throw message;
@@ -61,20 +81,21 @@ module.exports = {
         }
     },
 
-    async updateVehicleType(label, description, id){
+    async updateFormula(label, description, formulaTypeId, id, transaction){
         try {
-            const result = await db.query(UPDATE_VEHICLE_TYPE, [label, description, id]);
+            const result = await db.query(UPDATE_FORMULA, [label, description, formulaTypeId, id]);
 
             if(result.affectedRows !== 1){
-                console.error("Update vehicle_type went bad. The expected number of affected rows is wrong."
+                console.error("Update formula went bad. The expected number of affected rows is wrong."
                     + " Expected: 1, affected rows: " + result.affectedRows);
                 return false;
             }
             return true;
         }
         catch(err){
-            const message = '[vehiclesTypes.db.service] Updating vehicle type with values { label: '
-                + label + ', description: ' + description + ', id: ' + id + ' } went bad. Reason: ' + err;
+            const message = '[formulas.db.service] Updating formula type with values { label: '
+                + label + ', description: ' + description + ', formulaTypeId: ' + formulaTypeId
+                + ', id: ' + id + ' } went bad. Reason: ' + err;
 
             console.error(message);
             throw message;
@@ -84,19 +105,19 @@ module.exports = {
         }
     },
 
-    async deleteVehicleType(id){
+    async deleteFormula(id, transaction){
         try {
-            const result = await db.query(DELETE_VEHICLE_TYPE, [id]);
+            const result = await db.query(DELETE_FORMULA, [id]);
 
             if(result.affectedRows !== 1){
-                console.error("Delete vehicle type went bad. The expected number of affected rows is wrong."
+                console.error("Delete formula went bad. The expected number of affected rows is wrong."
                     + " Expected: 1, affected rows: " + result.affectedRows);
                 return false;
             }
             return true;
         }
         catch(err){
-            const message = '[vehiclesTypes.db.service] Deleting vehicle type with value { id: '
+            const message = '[formulas.db.service] Deleting formula with value { id: '
                 + id + ' } went bad. Reason: ' + err;
 
             console.error(message);
---
+++ src/pages/api/formulas-full-informations/index.js
@@ -0,0 +1,54 @@
+const formulasTasksDB = require('../../../database/formulasTasks.db.service');
+
+const FORMULA_TYPE_READY_ID = 1
+
+export default async function handler(req, res){
+    const method = req.method;
+
+    switch(method){
+        case 'GET': return handleGet(req, res);
+        default:
+            res.setHeader('Allow', ['GET', 'PUT']);
+            res.status(405).json({
+                data: null,
+                error: { message: 'Method ' + method + ' not allowed' }
+            });
+    }
+}
+
+const handleGet = async (req, res) => {
+    try {
+        const formulasJoinTasks = await formulasTasksDB.getFormulasJoinTasksByFormulaTypeId(FORMULA_TYPE_READY_ID);
+        let formulasFullInformations = []
+
+        formulasJoinTasks.forEach(formulaJoinTask => {
+            if(!formulasFullInformations.find(formula => formula.formulaId === formulaJoinTask.formula_id)){
+                formulasFullInformations.push({
+                    formulaId: formulaJoinTask.formula_id,
+                    formulaLabel: formulaJoinTask.formula_label,
+                    tasks: [
+                        {id: formulaJoinTask.task_id, type: formulaJoinTask.type, price: formulaJoinTask.price}
+                    ]
+                })
+            }
+            else{
+                let formula = formulasFullInformations
+                    .find(formulaFullInformations => formulaFullInformations.formulaId === formulaJoinTask.formula_id)
+                formula.tasks.push({id: formulaJoinTask.task_id, type: formulaJoinTask.type, price: formulaJoinTask.price})
+            }
+        })
+
+        return res.status(200).json({
+            data: { formulas: formulasFullInformations },
+            error: null
+        });
+    }
+    catch(err){
+        const message = '[formulas] ' + err;
+        console.error(message);
+        res.status(500).json({
+            data: null,
+            error: { message: message }
+        });
+    }
+}
