ea42b4c0ee849f92760a20d19d58f6196f55a633
MOUNCIF <Mons-L@users.noreply.github.com>
2023-07-02 20:25:48 +0200

    get ORDERS

--- src/database/orders.db.service.js
+++ src/database/orders.db.service.js
@@ -3,7 +3,7 @@ import db from './db.service';
 const SELECT_ORDERS = 'SELECT * FROM orders';
 const SELECT_ORDERS_BY_USER_ID = 'SELECT * FROM orders WHERE userId=?';
 const SELECT_ORDERS_JOIN_PRODUCT_BY_USER_ID = 'SELECT o.id AS orderId, o.orderDate, o.deliveryType, o.deliveryCharges, o.paymentMode, o.status, p.id AS productId, p.label, p.imagePath, p.price, op.quantity FROM orders o INNER JOIN orders_product op on o.id = op.orderId INNER JOIN product p on p.id = op.productId WHERE userId=?';
-const SELECT_ORDERS_JOIN_ADRESSES_BY_ORDER_ID = 'SELECT a.id, a.fullname, a.number, a.label, a.phone, oa.type AS addressType FROM orders o INNER JOIN orders_address oa on o.id = oa.orderId INNER JOIN address a on a.id = oa.addressId WHERE orderId=?';
+const SELECT_ORDERS_JOIN_ADRESSES_JOIN_CITY_BY_ORDER_ID = 'SELECT a.id, a.fullname, a.number, a.label, a.phone, oa.type addressType, c.label city, c.postalCode FROM orders o INNER JOIN orders_address oa on o.id = oa.orderId INNER JOIN address a on a.id = oa.addressId INNER JOIN city c ON c.id=a.cityId WHERE orderId=?';
 
 
 module.exports = {
@@ -51,7 +51,7 @@ module.exports = {
 
     async getOrderJoinAddressesByOrderId(orderId){
         try {
-            return await db.query(SELECT_ORDERS_JOIN_ADRESSES_BY_ORDER_ID, [orderId]);
+            return await db.query(SELECT_ORDERS_JOIN_ADRESSES_JOIN_CITY_BY_ORDER_ID, [orderId]);
         }
         catch(err){
             const message = '[orders.db.service] Getting order join addresses by order id went bad. Reason: ' + err;
